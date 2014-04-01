efceb5a770bd36d44e547d601a9a8087040400b3
Frank Li <lxf20035@msn.com>
2014-04-01 09:54:06 -0400

    added 3 final scene versions

--- cs488-proj/final_3.lua
+++ cs488-proj/final_3.lua
@@ -286,5 +286,5 @@ gr.render(scene,
       'final.png', 1000, 1000,
       {0, 0, 10}, {0, 0, -1}, {0, 1, 0}, 50,
       {0.4, 0.4, 0.4}, {
-        gr.light({0, 2.48, 3.167}, {0.6, 0.6, 0.6}, {1, 0, 0}, 5000000, 107, 1, 'square', 1.5) -- pos, colour, attenuation, num photons, power of initial photons, light type ('square','round','point'), power bias, size(if non point light).
-      }, 3, 0.1, 600, 'photon map', 32, 'pinhole', 180.0, 1) -- num recur, search radius, num photons per search area, render mode, num threads, eye mode, max psi for fisheye, num sqrt(samples) per pixel for stochastic rendering
+        gr.light({0, 2.48, 3.167}, {0.6, 0.6, 0.6}, {1, 0, 0}, 400000000, 110, 1, 'square', 1.5) -- pos, colour, attenuation, num photons, power of initial photons, light type ('square','round','point'), power bias, size(if non point light).
+      }, 6, 0.03, 8000, 'photon map', 32, 'pinhole', 180.0, 1) -- num recur, search radius, num photons per search area, render mode, num threads, eye mode, max psi for fisheye, num sqrt(samples) per pixel for stochastic rendering
